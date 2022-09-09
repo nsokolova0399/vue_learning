26f4dcc222f9761b90c7fa2994c1b31d83c581fe
nsokolova0399 <nsokolova0399@mail.ru>
2022-09-09 12:54:22 +0300

    Change index

--- src/components/UI/index.js
+++
@@ -1,8 +0,0 @@
-import MyButton from "../UI/MyButton";
-import MyInput from "../UI/MyInput";
-import MyDialog from "../UI/MyDialog";
-import MySelect from "../UI/MySelect";
-import MyPage from "../UI/MyPage";
-export default [
-    MyButton, MyInput, MyDialog, MySelect, MyPage
-]
