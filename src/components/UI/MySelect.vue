e35482ff6c70e0e6f5fab1026c5bf425cf75f3e3
nsokolova0399 <nsokolova0399@mail.ru>
2022-09-09 12:57:30 +0300

    Adding MySelect

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,44 @@
+<template>
+    <select class="listSelected" v-model="modelValue" @change="changeOption">
+        <option disabled value="">Выберите из списка</option>
+        <option
+                v-for="option in options"
+                :key="option.value"
+                :value="option.value"
+        >
+            {{ option.name }}
+        </option>
+    </select>
+</template>
+
+<script>
+    export default {
+        name: "my-select",
+        props:{
+            modelValue: {
+                type: String
+            },
+            //объекты массива
+            options: {
+                type: Array,
+                //для пустых массивов реккомендуется использовать стрелочную функцию
+                default: () => []
+            }
+        },
+        methods:{
+            changeOption(event){
+                this.$emit('update:modelValue', event.target.value);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.listSelected{
+    border-color: teal;
+    border-width: 3px;
+    padding: 15px;
+    color: #004949;
+    font-size: 15px;
+}
+</style>
